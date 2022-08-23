ecc0aeefbd88b899efdfb7560933eaedf31f4a49
serdalaydin6116 <serdalaydin.6116@gmail.com>
2022-08-23 23:23:16 +0300

    jped was added as static file

--- views/views.py
+++ views/views.py
@@ -7,13 +7,21 @@ from django.http import HttpResponse
 #     html ="<h1>This is entrance to view lecture</h1>"
 #     return HttpResponse(html)
 
-def home(request):
-    print(request)
-    print(request.method)
-    print(request.COOKIES)
-    print(request.path)
-    print(request.user)
-    print(request.META)
+# def home(request):
+#     print(request)
+#     print(request.method)
+#     print(request.COOKIES)
+#     print(request.path)
+#     print(request.user)
+#     print(request.META)
    
-    html ="<h1>This is entrance to view lecture</h1>"
-    return HttpResponse(html)
+#     html ="<h1>This is entrance to view lecture</h1>"
+#     return HttpResponse(html)
+
+def home(request):
+    context = {
+        'caption': 'clarusway',
+        'dict1': {'django' : 'best framework'},
+        'my_list' : [2,3,4]
+    }
+    return render(request, 'views/index.html', context)
