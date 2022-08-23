8f073a2084c7000fba51f62dac1fe33276c37c81
serdalaydin6116 <serdalaydin.6116@gmail.com>
2022-08-23 23:21:42 +0300

    temporary app was created

---
+++ temporary/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.http import HttpResponse
+
+# def home(request):
+#     html ="<h1>This is entrance to view lecture</h1>"
+#     return HttpResponse(html)
+
+# def home(request):
+#     print(request)
+#     print(request.method)
+#     print(request.COOKIES)
+#     print(request.path)
+#     print(request.user)
+#     print(request.META)
+   
+#     html ="<h1>This is entrance to view lecture</h1>"
+#     return HttpResponse(html)
+
+def home(request):
+    return render(request, 'temporary/index.html')
