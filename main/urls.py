c4f05ab76533fce4cbe3b2b3ce20ed811f326458
serdalaydin6116 <serdalaydin.6116@gmail.com>
2022-08-23 23:19:46 +0300

    main/settings and urls were formed

--- main/urls.py
+++ main/urls.py
@@ -20,4 +20,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('view', include('views.urls')),
+    path('tempo', include('temporary.urls')),
+   
 ]
